a46bb26e5ff6448a168363e1feaccd6d50731817
martin0230206 <martin0230206@gmail.com>
2024-02-21 22:40:31 +0800

    feat: ✨ 附檔限制25MB以下 #1

--- libs/email/router.py
+++ libs/email/router.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 from typing import List
 
-from fastapi import APIRouter, Body, File, HTTPException, UploadFile
+from fastapi import APIRouter, Body, File, HTTPException, UploadFile, status
 from fastapi.responses import JSONResponse
 
 from .core import send_email
-from .schema import EmailPayload
+from .schema import ATTACHMENT_MAX_TOTAL_SIZE, EmailPayload
 
 router = APIRouter()
 
@@ -18,6 +18,12 @@ async def post_email(
     email_payload: EmailPayload = Body(...),
     upload_file_list: List[UploadFile] = File(None, description="附加檔案"),
 ):
+    if sum(upload_file.size for upload_file in upload_file_list) > ATTACHMENT_MAX_TOTAL_SIZE:
+        raise HTTPException(
+            status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+            detail="附加檔案總大小不可超過25MB"
+        )
+
     attachment_filename_list = [
         upload_file.filename
         for upload_file in upload_file_list
